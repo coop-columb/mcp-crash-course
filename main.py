6b917cad56bee485c9f4c9f1a39c404d79efa1e5
coop-columb <ahc2188@columbia.edu>
2025-07-08 20:33:39 -0400

    Enhance main application and update dependencies
    - Refactored main.py to include new imports and set up a ChatOpenAI instance.
    - Added StdioServerParameters for the math server.
    - Updated pyproject.toml to include isort as a dependency.
    - Modified uv.lock to reflect the addition of isort version 6.0.1.
    - Minor update to weather_server.py for consistency.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import asyncio
-from dotenv import load_dotenv
 import os
 
+
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY")) # This will print the API key to the console
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command = "python",
+    args = ["/Users/root1/udemy-mcp-course/mcp-crash-course/servers/math_server.py"],
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
-
 
 if __name__ == "__main__":
     asyncio.run(main())
